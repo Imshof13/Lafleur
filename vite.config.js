ae50f40e0cfaf35fe333ce1c201c9db4b5704ef6
Imshof13 <imtiyazshofwan@gmail.com>
2026-06-14 22:27:46 +0700

    Configure Vercel to serve Laravel routes

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            publicDirectory: '.',
-            buildDirectory: 'dist',
             refresh: true,
         }),
         tailwindcss(),
