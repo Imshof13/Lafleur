a64523eed5ed28f478e5838505ee06f5a4b70699
Imshof13 <imtiyazshofwan@gmail.com>
2026-06-15 06:52:26 +0700

    Register view provider before app boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Cabang;
 use Illuminate\Support\Facades\View;
+use Illuminate\View\ViewServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->register(ViewServiceProvider::class);
     }
 
     /**
